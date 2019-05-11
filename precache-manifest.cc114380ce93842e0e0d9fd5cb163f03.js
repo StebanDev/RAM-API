013bc00b7598cd877dc9e9c7053d4e758876a332
Esteban C <eciudadconde@gmail.com>
2019-05-11 16:43:29 -0500

    Updates

---
+++ precache-manifest.cc114380ce93842e0e0d9fd5cb163f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb23e8c8d607e0457648",
+    "url": "./static/css/main.16642a28.chunk.css"
+  },
+  {
+    "revision": "bb23e8c8d607e0457648",
+    "url": "./static/js/main.bb23e8c8.chunk.js"
+  },
+  {
+    "revision": "41fd6703131b4d7e91ff",
+    "url": "./static/js/1.41fd6703.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "./static/media/logo.42d6efdb.png"
+  },
+  {
+    "revision": "69a2f4754af04d4aa52627d22cc68ab0",
+    "url": "./index.html"
+  }
+];
